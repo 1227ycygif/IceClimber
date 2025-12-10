54a2f510febb852fd336a25c6315e0455768591e
1227ycygif <1227ycy@kyonggi.ac.kr>
2025-12-10 18:12:51 +0900

    프로젝트 전체 중 BGM 4개 빼고

---
+++ FrameWork/Device.cpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "Include.h"
+
+bool Device::Create(IDirect3DDevice9* Device)
+{
+    Device9 = Device;
+
+    D3DXCreateSprite(Device9, &Sprite);
+
+    ZeroMemory(&fdesc, sizeof(fdesc));
+    fdesc.Height = 25;
+    fdesc.Width = 12;
+    fdesc.Weight = 500;
+    fdesc.Italic = FALSE;
+    fdesc.CharSet = DEFAULT_CHARSET;
+
+    // ±Û²Ã ¼³Á¤ ("µ¸¿ò")
+    _tcscpy_s(fdesc.FaceName, _T("µ¸¿ò"));
+
+    D3DXCreateFontIndirect(Device9, &fdesc, &Fonts);
+
+    return true;
+}
+
+bool Device::DrawString(const TCHAR* msg, int x, int y, D3DCOLOR color)
+{
+    RECT rect = { x, y, x + (int)_tcslen(msg) * fdesc.Width, y + fdesc.Height };
+
+    Sprite->Begin(D3DXSPRITE_ALPHABLEND);
+    Fonts->DrawText(Sprite, msg, -1, &rect, DT_NOCLIP, color);
+    Sprite->End();
+
+    return true;
+}
